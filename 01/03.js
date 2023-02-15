8e0b83c0df827b0b022f6a4435c808131c1ea3be
Anna Dubik <anngwork@gmail.com>
2023-02-15 22:57:17 +0300

    task: 01.4

---
+++ 01/03.js
@@ -0,0 +1,11 @@
+// https://learn.javascript.ru/types#tasks
+
+// Что выведет этот скрипт?
+
+let name = "Ilya";
+
+console.log( `hello ${1}` ); // hello 1
+
+console.log( `hello ${"name"}` ); // hello name
+
+console.log( `hello ${name}` ); // hello Ilya
