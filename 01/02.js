2a6cb8cc84e98f6f538a3b3b2805c7553d796dd4
Anna Dubik <anngwork@gmail.com>
2023-02-15 21:39:50 +0300

    task: 01.3

---
+++ 01/02.js
@@ -0,0 +1,18 @@
+// https://learn.javascript.ru/variables#tasks
+
+// Task 01
+let admin;
+let name;
+
+name = "Джон";
+admin = name;
+
+alert(admin);
+
+
+// Task 02
+// Создайте переменную для названия нашей планеты.
+let ourPlanet;
+
+// Создайте переменную для хранения имени текущего посетителя сайта.
+let currentUserName;
